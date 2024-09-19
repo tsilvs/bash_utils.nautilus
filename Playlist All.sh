c34abeaaca0b1e30d476eb4c8c4ff916897f9225
Vsevolod Tsiliurik <tsilvs@proton.me>
2024-09-19 12:59:24 +0100

    Playlist all media files in subdirectories script

---
+++ Playlist All.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+prop_get() {
+	local json="$1"
+	local prop="$2"
+	echo "$json" | jq -r "$prop"
+}
+
+main() {
+	local lang="en"
+	local timeout='10000'
+	local uri="$NAUTILUS_SCRIPT_CURRENT_URI"
+	local i18n='
+	{
+		"en": {
+			"succ": {
+				"summ": "File Generated",
+				"body": "Playlist has been created successfully."
+			},
+			"fail": {
+				"summ": "Error",
+				"body": "Playlist making failed. No media files found."
+			},
+			"open": "Open File",
+			"del": "Delete File"
+		}
+	}
+	'
+	
+	local types=( "wav" "wv" "flac" "ogg" "mp3" "mp4" "avi" "mkv" )
+	
+	local dirpath="${uri#file://}"
+	dirpath="${dirpath//%20/ }"
+	local dirname="${dirpath##*/}"
+	
+	local playlist_file="$dirpath/$dirname-Full.m3u"
+	echo "#EXTM3U" > "$playlist_file"
+
+	local find_cmd="find \"$dirpath\" -type f \("
+	for i in "${!types[@]}"; do
+		[[ $i -gt 0 ]] && find_cmd+=" -o"
+		find_cmd+=" -iname \"*.${types[i]}\""
+	done
+	find_cmd+=" \)"
+
+	local file_count=0
+	local found_files=""
+	while read -r file; do
+		rel_path="${file#$dirpath/}"
+		found_files+="$rel_path\n" 
+		((file_count++))
+	done < <(eval "$find_cmd")
+
+	echo -e $found_files | sort >> "$playlist_file"
+	
+	if [ $file_count -gt 0 ]; then
+		act=$(
+			notify-send \
+				"$(prop_get "$i18n" ".$lang.succ.summ")" \
+				"$(prop_get "$i18n" ".$lang.succ.body")" \
+				-A "open=$(prop_get "$i18n" ".$lang.open")" \
+				-A "del=$(prop_get "$i18n" ".$lang.del")" \
+				--wait \
+				-t $timeout
+		)
+		case "$act" in
+			"open") xdg-open "$playlist_file" ;;
+			"del") 
+				if [ -f "$playlist_file" ]; then
+					rm "$playlist_file"
+				fi
+				;;
+		esac
+	else
+		notify-send \
+			"$(prop_get "$i18n" ".$lang.fail.summ")" \
+			"$(prop_get "$i18n" ".$lang.fail.body")"
+		[ -f "$playlist_file" ] && rm "$playlist_file"
+	fi
+	return 0
+}
+
+main "$@"
